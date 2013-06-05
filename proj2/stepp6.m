e7fd6a377a48a36a26165abe05e9f223574a9ea8
Nathan Garvey <garvey.na@gmail.com>
2013-06-04 23:27:02 -0700

    ready for final submission

--- proj2/stepp6.m
+++ proj2/stepp6.m
@@ -2,7 +2,7 @@
 %ELEN 100 2013
 %check for problem 6
 %transient analysis of a circuit with chosen values
-% peak overshoot of < 10%
+% values give peak overshoot of < 10%
 
 clear all;
 syms R1 R2 R3 R4 R5
@@ -44,51 +44,3 @@ figure(1);
 plot(tt,v0n,'g',tt,v1n,'b',tt,v2n,'r'); grid on; title('Step Response R1 = R2 = 1.2k\Omega  R3 = 838\Omega, R4 = R5 = 1k\Omega');
 legend('V_o = Voutput');xlabel('Time [s] '); ylabel('Vo [V]');
 grid on; 
-
-
-if 0
-	 % ELEN 100 - Project 2
-
-% Tutorial 
-%close all;
-clear all;
- 
-% Symbolic variables 
-syms R1 R2 R3 
-syms C
-syms s t
- 
-% Real values
-r1 = 2;
-r2 = 2;
-r3 = 1;
-c  = 1/2;
- 
-% Matrix in Laplace form
-M = [1/R1 + 1/R2 + 1/R3, -1/R3; -1/R3, 1/R3+s*C];
-S = [1/(s*R1) ; 0];
- 
-% Solving the matrix
-X = M\S;
-Xn = subs (X, {R1 R2 R3 C} , {r1 r2 r3 c} );
- 
-% Finding Inverse Laplace
-x = ilaplace(Xn);
- 
-v1 = x(1);
-v2 = x(2);
- 
-% Calculate transient response
-tt = [0: .1 : 5];
- 
-for i = 1:length(tt) 
-    v1n (i) = subs ( v2 , {t} , {tt(i)});
-    v2n (i) = subs ( v1 , {t} , {tt(i)});
-end
- 
-% Plotting
-figure(2);
-plot(tt,v1n,tt,v2n); grid on; title('Step Response R1 = R2 = 1.2k\omega R3 = 838\omega, R4 = R5 = 1k\omega');
-legend('V_C = Voutput','V_R_1');xlabel('Time [s] '); ylabel('Vo [V]');
-grid on; 
-end;
